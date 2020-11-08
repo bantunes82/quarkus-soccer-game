7d14cae6abc2ce942014d832fb902606aa0d433b
bruno.romao <bruno.romao@edreamsodigeo.com>
2020-11-08 14:23:21 +0100

    Creating Integration Test for Native mode

--- rest-team/src/test/java/quarkus/soccer/game/team/controller/NativeTeamControllerIntegrationTest.java
+++ rest-team/src/test/java/quarkus/soccer/game/team/controller/NativeTeamControllerIntegrationTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.NativeImageTest;
 import org.junit.jupiter.api.Tag;
 
 @NativeImageTest
-@Tag("integration")
+@Tag("native")
 public class NativeTeamControllerIntegrationTest extends TeamControllerIntegrationTest {
     // Execute the same tests but in native mode.
 }
